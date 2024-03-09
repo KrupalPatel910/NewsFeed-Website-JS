4224fc5107abcab8f451f3c049268e27e8adeecc
Krupal Patel <krupal9102001@gmail.com>
2024-03-09 15:24:44 +0530

    Navbar added

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('This is project 3');
+
+
+// API fetching                 
+
+// async function apicall(){
+//     const  response = await fetch('https://dummyjson.com/products')
+//     const result= await response.json();
+//     console.log(result.products);
+
+// }
+
+// apicall();
+
